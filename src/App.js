a054afb429c9d67d05bd3dc159cc0209faa3ce14
vision <karohambaryan@gmail.com>
2022-09-16 01:03:46 +0400

    Մինչև Կոնտեյներ ստեղծելը

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	state = {
+		books: []
+	}
+
+	componentDidMount() {
+		fetch("data/books.json")
+			.then(response => response.json())
+			.then(data => {
+				this.setState({ books: data.items })
+			})
+	}
+
+	render() {
+		const { books } = this.state
+		console.log(books);
+		return (
+			<div id='main'>
+				<h1> Welcome to Book Store </h1>
+				<div id='books'>
+					{
+						books.map((elm, index) => {
+							return (
+								<div key={elm.id}>
+									<img src={elm.pic} alt="altfoto" />
+									<h5>{elm.title}</h5>
+									<p>By { elm.author }</p>
+								</div>
+							);
+						})
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
